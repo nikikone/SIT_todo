dd4f650b773e85cb2196d324b27a3effa4c5b0a4
nikikone <87607801+nikikone@users.noreply.github.com>
2022-01-28 14:09:19 +1000

    update models

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class File(models.Model):
     file = models.FileField(blank=False, null=False)
-    remark = models.CharField(max_length=20)
+    remark = models.CharField(max_length=40)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
